a577983495ac88278676a4e725413db83db8e2ff
Anas Ambri <anas.ambri@gmail.com>
2013-10-31 20:07:56 -0400

    Simpler data for testing

--- test/test_data.js
+++ test/test_data.js
@@ -1,7 +1,7 @@
 module.exports = {
     longSequence : {
 	request : {
-	    sequence : 'AUUGUACGCGUCAUGCAUAGCACGAUGCAUGCUC',
+	    sequence : 'AUUGUACGCGUCGCACGAUGCAUGCUC',
 	    foldShape : 'Basic',
 	    temperature : 37,
 	    naC: 150,
@@ -19,29 +19,6 @@ module.exports = {
 		target: ''
 	    }
 	},
-	results : {},
-	duration : {
-	    remainingDuration : 234,
-	    unit: 'min'
-	},
-	newRequest: {
-	    sequence : 'ATGC',
-	    foldShape : 'Basic',
-	    temperature : 37,
-	    naC: 150,
-	    mgC: 0,
-	    oligoC: 0,
-	    cutsites: ['GUC'],
-	    region: '5\'',
-	    left_arm_min : 3,
-	    left_arm_max : 8,
-	    right_arm_min : 3,
-	    right_arm_max : 8,
-	    emailUser : 'test@test.test',
-	    env: {
-		type: 'vitro',
-		target: ''
-	    }
-	}
+	results : {}
     }
 };
